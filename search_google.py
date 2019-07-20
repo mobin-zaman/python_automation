4409512a810113946fe923ac313281185d6a4daf
mobin-zaman <38745180+mobin-zaman@users.noreply.github.com>
2019-07-20 12:52:20 +0600

    Update search_google.py

--- search_google.py
+++ search_google.py
@@ -2,6 +2,8 @@
 import clipboard
 import webbrowser
 
+''' install dependencey:  sudo apt install xsel '''
+
 url = "https://www.google.com.tr/search?q=" + clipboard.paste()
 webbrowser.open(url)
 exit()

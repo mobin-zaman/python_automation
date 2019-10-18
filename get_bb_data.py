3954df96242c2d98b6d255ba92185bef53e9046f
mobin-zaman <38745180+mobin-zaman@users.noreply.github.com>
2019-10-18 11:02:25 +0600

    Add files via upload

--- get_bb_data.py
+++ get_bb_data.py
@@ -1,48 +1,65 @@
 from datetime import datetime
 import requests
 
-today = datetime.today()
 
-print(today.year)
-print(today.month)
+def get_latest_xlsx(number_of_months):
 
-#https://bb.org.bd/pub/monthly/econtrds/aug19/statisticaltable.xlsx
+    today = datetime.today()
 
+    print(today.year)
+    print(today.month)
 
-months=['null','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
-
-base_url="https://bb.org.bd/pub/monthly/econtrds/"
-year_string=str(today.year)
-genarated_url=base_url+months[today.month]+year_string[:2]+'/statisticaltable.xlsx';
-print(genarated_url)
-
-print("beginning download")
-
-#url = "https://bb.org.bd/pub/monthly/econtrds/aug19/statisticaltable.xlsx"
-response= requests.get(genarated_url)
-print(response.status_code)
-
-if response.status_code!=200:
-    if today.month!=1:
-        genarated_url=base_url+months[today.month-1]+year_string[2:]+'/statisticaltable.xlsx';
-        print(genarated_url)
-    else:
-        prev_year_int=today.year-1
-        prev_year=str(prev_year_int)
-
-        genarated_url=base_url+months[12]+prev_year[2:]+'/statisticaltable.xlsx';
-        print(genarated_url)
-    
-    response=requests.get(genarated_url)
-
-    if response.status_code!=200:
-        print("downloading failed")
-        exit()
-    
+    #https://bb.org.bd/pub/monthly/econtrds/aug19/statisticaltable.xlsx
 
 
-#the below line saves the binary data, it be returned as well
-with open('latest.xlsx','wb') as f:
-    f.write(response.content)
+    months=['null','jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 
-print("success")
+    base_url="https://bb.org.bd/pub/monthly/econtrds/"
+
+    year=today.year
+    year_string=str(today.year)
+
+
+    month_iterator = today.month
+
+    counter=0
+
+
+
+
+    while(number_of_months!=-1):
+        
+        if months[month_iterator] == 'null':
+            month_iterator=12
+            year-=1
+            year_string=str(year)
+        
+        print("beginning download: "+months[month_iterator])
+
+        genarated_url=base_url+months[month_iterator]+year_string[2:]+'/statisticaltable.xlsx';
+
+        #url = "https://bb.org.bd/pub/monthly/econtrds/aug19/statisticaltable.xlsx"
+        response= requests.get(genarated_url)
+        print('status: ',response.status_code)
+
+        month_iterator-=1
+        number_of_months-=1
+
+     
+        if response.status_code!=200:
+            number_of_months+=1
+        
+        else:
+            #latest month is saved as 1.xlsx, then 2.xlsx, 3.xlsx so on, in a descending order
+            counter+=1
+            with open(str(counter)+'.xlsx','wb') as f:
+                print(str(counter)+'.xlsx', ' downloaded')
+                f.write(response.content)
+        
+
+        
+
+"""exmple function call for last 13 months"""
+get_latest_xlsx(13)
+
+
